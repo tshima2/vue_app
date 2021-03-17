69b943688cffe0aa1b588c1c102cca82b564ca4a
SHIMA Tatsurou <shima.tatsurou@gmail.com>
2021-03-17 19:56:43 +0900

    「入力した生徒を追加する」というボタンを押した時に、名前とコースと受講期の入力欄を空にする機能の追加

--- vue_app.js
+++ vue_app.js
@@ -20,16 +20,24 @@ var app2=new Vue({
     },
     methods: {
         insertClient: function() {
+	    if(!client_name.value){
+		alert('名前を入力して下さい');
+		return;
+	    }
             ha={};
             ha["text"]="ID 1 /名前 "+client_name.value+" /コース " + client_course.value + "/受講期 " + client_term.value;
-            
+                
             //clients.push(ha);
             //app2.clients.push(ha);
             this.clients.push(ha);
-            
-            app1.client_name="";
-            app1.client_course="";
-            app1.client_term="";            
+                
+	    client_name.value=""
+	    client_course.value=""
+	    client_term.value=""	    
+	    //app1.client_name=""
+	    //app1.client_course=""
+	    //app1.client_term=""	    
+
         }
     }
 });
